7ebadb56dc8b9da3fee1b09fd624a8ee687395a8
mohamed99elsokary <mohamed99elsokary@gmail.com>
2021-08-15 04:32:26 +0200

    first commit

---
+++ profiles/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class profile(models.Model):
+    # -------relations------- #
+    address = models.ForeignKey("addresses", on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    # -------fields------- #
+    name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+
+class addresses(models.Model):
+    # -------fields------- #
+    country = models.CharField(max_length=20)
+    city = models.CharField(max_length=20)
+    street_name = models.CharField(max_length=20)
+    bulding_number = models.CharField(max_length=20)
+    floor_number = models.CharField(max_length=20)
+    apartment_number = models.CharField(max_length=20)
+
+    def __str__(self):
+        name = f"{self.country}  {self.city}  {self.street_name}  {self.bulding_number}  {self.floor_number} {self.apartment_number}"
+        return name
---
+++ project.md
@@ -0,0 +1,32 @@
+User
+    email
+    password
+
+Token
+    user_id
+    token
+
+profile
+    user_id
+    name
+    address_id
+    phone
+
+address
+    country
+    city
+    street_name
+    bulling_number
+    flor number
+    apartment_number
+
+products
+    name
+    quantity
+    buy_price
+    sell_price
+    brand
+    category_id
+
+categories
+    name
